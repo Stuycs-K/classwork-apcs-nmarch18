58d988788d9c665d7cada10e638ada4e07d25430
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-01 00:52:25 -0400

    Added test cases for arrToString2, arr2DSum

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -24,55 +24,83 @@ public class ArrayMethods{
     return str;
   }
 
-//3. Write arrToString, with a 2D array parameter.
-//Note: Different parameters are allowed with the same function name.
-/**Return a String that represets the 2D array in the format:
+  //3. Write arrToString, with a 2D array parameter.
+  //Note: Different parameters are allowed with the same function name.
+  /**Return a String that represets the 2D array in the format:
   * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
   * Note the comma+space between values, and between arrays
   * You are encouraged to notice that you may want to re-use
   * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
   */
-public static String arrToString2(int[][]ary){
-  String str = "["
-  for (int i = 0; i < ary.length; i++){
-    if (i < ary.length - 1){
-      str += arrToString(ary[i]) + ", ";
-    }
-    else {
-      str += arrToString(ary[i]);
-    }
+  public static String arrToString2(int[][]ary){
+    String str = "[";
+    for (int i = 0; i < ary.length; i++){
+      if (i < ary.length - 1){
+        str += arrToString(ary[i]) + ", ";
+      }
+      else {
+        str += arrToString(ary[i]);
+      }
+  } 
+    return str + "]";
   }
-  return str + "]";
-}
 
-/*Return the sum of all of the values in the 2D array */
-public static int arr2DSum(int[][]nums){
+  /*Return the sum of all of the values in the 2D array */
+  public static int arr2DSum(int[][]nums){
   //use a nested loop to solve this
-  int sum = 0;
-  for (int x = 0; x < nums.length; x++) {
-    for (int i = 0; i < nums[x].length; i++) {
-      sum += nums[x][i];
+    int sum = 0;
+    for (int x = 0; x < nums.length; x++) {
+      for (int i = 0; i < nums[x].length; i++) {
+        sum += nums[x][i];
+      }
     }
+    return sum;
   }
-  return sum;
-}
 
-/**Rotate an array by returning a new array with the rows and columns swapped.
+  /**Rotate an array by returning a new array with the rows and columns swapped.
   * You may assume the array is rectangular and neither rows nor cols is 0.
   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   */
-public static int[][] swapRC(int[][]nums){
-  int[][] arr = new int[nums[0].length][nums.length];
-  for (int i = 0; i < nums[0].length; i++){
-    for (int j = 0; i < nums.length; j++){
-      arr[i][j] = nums[j][i];
+  public static int[][] swapRC(int[][]nums){
+    int[][] arr = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums[0].length; i++){
+      for (int j = 0; i < nums.length; j++){
+        arr[i][j] = nums[j][i];
+      }
     }
+    return arr;
   }
-  return arr;
-}
 
-}
+  public static void main(String[] args){
+    int[] arr;
+    int[][] arr2D;
 
-public static void main(String[] args){
+    arr = new int[]{1, 2, 3, 4};
+    System.out.println("arrToString test: " + arrToString(arr) + "\n");
+    
+    // 3 test cases for arrToString2: expected the array in string form
+
+    arr2D = new int[2][4];
+    System.out.println("arrToString2 test 1: " + arrToString2(arr2D));
+
+    arr2D = new int[][]{{1, 2, 3}, {2, 3}, {3}, {}};
+    System.out.println("arrToString2 test 2: " + arrToString2(arr2D));
+
+    arr2D = new int[0][0];
+    System.out.println("arrToString2 test 3: " + arrToString2(arr2D) + "\n");
+
+    // 3 test cases for arr2DSum: expected sum of all the ints in the 2D array
+
+    arr2D = new int[][]{{3, 4, 2}, {8, 0, 1}, {5, 2, 2}, {6, 3, 0}, {1, 4, 4}};
+    System.out.println("arr2DSum test 1: " + arr2DSum(arr2D));
+
+    arr2D = new int[100][100];
+    System.out.println("arr2DSum test 2: " + arr2DSum(arr2D));
+
+    arr2D = new int[][]{{410}, {41, 0}, {4, 10}, {4, 1, 0}};
+    System.out.println("arr2DSum test 3: " + arr2DSum(arr2D));
+    
+    // 3 test cases for swapRC:
+  }
 
 }
