6397548b9fabadcfee9abf0368381cceba880a7f
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-07 10:59:45 -0400

    added previous test cases (END OF CLASS 10/7)

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -2,11 +2,74 @@ import java.util.Arrays;
 
 public class ArrayDemo{
   public static void main(String[]args){
-    //write your tests here!
-    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
-    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
-    //do not use any other Arrays.method()
+    int[] arr;
+    int[][] arr2D;
 
+    // 3 test cases for arrToString: expect the array in string form
+
+    arr = new int[]{1, 2, 3, 4};
+    System.out.println("\narrToString test 1: " + arrToString(arr));
+
+    arr = new int[]{};
+    System.out.println("arrToString test 2: " + arrToString(arr));
+
+    arr = new int[]{91, 24, 13, 4, 0, 9, 15, 62};
+    System.out.println("arrToString test 3: " + arrToString(arr) + "\n");
+
+    // 3 test cases for arrToString (2D): expect the array in string form
+
+    arr2D = new int[2][4];
+    System.out.println("arrToString 2D test 1: " + arrToString(arr2D));
+
+    arr2D = new int[][]{{1, 2, 3}, {2, 3}, {3}, {}};
+    System.out.println("arrToString 2D test 2: " + arrToString(arr2D));
+
+    arr2D = new int[0][0];
+    System.out.println("arrToString 2D test 3: " + arrToString(arr2D) + "\n");
+
+    // 3 test cases for arr2DSum: expect some int which is the sum
+
+    arr2D = new int[][]{{3, 4, 2}, {8, 0, 1}, {5, 2, 2}, {6, 3, 0}, {1, 4, 4}};
+    System.out.println("arr2DSum test 1: " + arr2DSum(arr2D));
+
+    arr2D = new int[100][100];
+    System.out.println("arr2DSum test 2: " + arr2DSum(arr2D));
+
+    arr2D = new int[][]{{410}, {41, 0}, {4, 10}, {4, 1, 0}};
+    System.out.println("arr2DSum test 3: " + arr2DSum(arr2D) + "\n");
+
+    // 3 test cases for swapRC: expect a 2d array w/ opposite dimensions and same values, in the right order
+
+    arr2D = new int[][]{{1, 2, 3}, {4, 5, 6}};
+    System.out.println("swapRC test 1: " + arrToString(swapRC(arr2D)));
+
+    arr2D = new int[][]{{1, 5, 9, 13}, {2, 6, 10, 14}, {3, 7, 11, 15}, {4, 8, 12, 16}};
+    System.out.println("swapRC test 2: " + arrToString(swapRC(arr2D)));
+
+    arr2D = new int[3][6];
+    System.out.println("swapRC test 3: " + arrToString(swapRC(arr2D)) + "\n");
+
+    // 3 test cases for replaceNegative: expect a slightly changed array from the original
+
+    arr2D = new int[][]{{-3, -3, 3, 3}, {-4, 4, -4}, {5, 5, -5, 5, -5}};
+    System.out.println("replaceNegative test 1: " + arrToString(replaceNegative(arr2D)));
+
+    arr2D = new int[][]{{-6}, {-6, 6}, {-6, 6, -6}, {-6, 6, -6, 6}};
+    System.out.println("replaceNegative test 1: " + arrToString(replaceNegative(arr2D)));
+
+    arr2D = new int[][]{{}, {}, {}, {2, -2, 2, -2, 2, -2}};
+    System.out.println("replaceNegative test 1: " + arrToString(replaceNegative(arr2D)) + "\n");
+
+    // 3 test cases for copy: expected unequal addresses and the same array when displayed as a string
+
+    arr2D = new int[][]{{1, 2, 3}, {4, 5}, {6}, {7, 8}, {9, 10, 11, 12}};
+    System.out.println("copy test 1: original address " + arr2D + "\t copy address " + copy(arr2D) + "\t copy array " + arrToString(copy(arr2D)));
+
+    arr2D = new int[4][3];
+    System.out.println("copy test 2: original address " + arr2D + "\t copy address " + copy(arr2D) + "\t copy array " + arrToString(copy(arr2D)));
+
+    arr2D = new int[][]{{},{0, 96, 0},{}};
+    System.out.println("copy test 3: original address " + arr2D + "\t copy address " + copy(arr2D) + "\t copy array " + arrToString(copy(arr2D)) + "\n");
   }
 
   //0. Include your prior methods to help you print a 1D/2D array of ints.
