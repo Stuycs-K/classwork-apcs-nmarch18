20e765e0770fbd0f6cd0e5418bfc6b9a372c1c70
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-09 00:21:35 -0400

    fixed arguments in some methods (DONE)

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -161,13 +161,13 @@ public class ArrayDemo{
   //-All other negatives replace with 0
 
   public static int[][] replaceNegative(int[][] vals){
-    for (int row = 0; row < vals.length; row++) { // treat vals[row][col] like a coordinate
-      for (int col = 0; col < vals[row].length; col++) {
-        if (row == col && vals[row][col] < 0) {
-          vals[row][col] = 1;
+    for (int r = 0; r < vals.length; r++) { // treat vals[row][col] like a coordinate
+      for (int c = 0; c < vals[r].length; c++) {
+        if (r == c && vals[r][c] < 0) {
+          vals[r][c] = 1;
         }
-        else if (vals[row][col] < 0) {
-          vals[row][col] = 0;
+        else if (vals[r][c] < 0) {
+          vals[r][c] = 0;
         }
       }
     }
@@ -185,9 +185,9 @@ public class ArrayDemo{
     for (int x = 0; x < nums.length; x++){
       newcopy[x] = new int[nums[x].length]; // make a matching non-rectangular array this way, setting each sub-array's length
     }
-    for (int i = 0; i < nums.length; i++){
-      for (int j = 0; j < nums[i].length; j++){
-        newcopy[i][j] = nums[i][j]; // then fill the array one by one in each corresponding index
+    for (int r = 0; r < nums.length; r++){
+      for (int c = 0; c < nums[r].length; c++){
+        newcopy[r][c] = nums[r][c]; // then fill the array one by one in each corresponding index
       }
     }
     return newcopy;
