f4cc7028e0d7a39b97e37b1c13c6dee24a537a76
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-15 11:05:24 -0400

    added static distance method in Point

--- 2024-10-15-points/Demo.java
+++ 2024-10-15-points/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return (Math.sqrt((Math.pow((a.getX() - b.getX()), 2.0)) + (Math.pow((a.getY() - b.getY()), 2.0))));
+    return (Math.sqrt((Math.pow(a.getX() - b.getX(), 2.0) + Math.pow(a.getY() - b.getY(), 2.0))));
   }
 
   public static void main(String[]args){
