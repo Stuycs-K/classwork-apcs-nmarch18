08ba12aaf7d65c4fc15c84a4391ffeb57476cfd2
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-15 11:08:13 -0400

    added test cases

--- 2024-10-15-points/Demo.java
+++ 2024-10-15-points/Demo.java
@@ -9,12 +9,26 @@ public class Demo{
   }
 
   public static void main(String[]args){
+    // case 1
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+
+    System.out.println( p1);
+    System.out.println( p2);
     System.out.println( p3);
-    System.out.println( distance(p1,p2));
+
+    System.out.println( distance(p2,p1));
     System.out.println( Point.distance(p1,p2));
-    System.out.println( p1.distanceTo(p2));
+    System.out.println( p2.distanceTo(p1));
+
+    System.out.println( distance(p1,p3));
+    System.out.println( Point.distance(p3,p1));
+    System.out.println( p1.distanceTo(p3));
+
+    System.out.println( distance(p3,p2));
+    System.out.println( Point.distance(p2,p3));
+    System.out.println( p3.distanceTo(p2));
+
   }
 }
