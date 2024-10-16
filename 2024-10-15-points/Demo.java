14188334a363f62853783b35dd15bfddc6d5bf49
Nicolas Marchese <nicktmarchese@gmail.com>
2024-10-16 00:54:58 -0400

    added test cases to verify outputs

--- 2024-10-15-points/Demo.java
+++ 2024-10-15-points/Demo.java
@@ -9,26 +9,31 @@ public class Demo{
   }
 
   public static void main(String[]args){
-    // case 1
+    // make point objects
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
 
+    // TESTS
+    System.out.println("toString test: expected coordinates (1.0, 1.0), (-1.0, -1.0), (3.0, 4.0)");
     System.out.println( p1);
     System.out.println( p2);
-    System.out.println( p3);
+    System.out.println( p3 + "\n");
 
+    System.out.println("distance methods test1: expected sqrt8 (~2.8284)");
     System.out.println( distance(p2,p1));
     System.out.println( Point.distance(p1,p2));
-    System.out.println( p2.distanceTo(p1));
+    System.out.println( p2.distanceTo(p1) + "\n");
 
+    System.out.println("distance methods test2: expected sqrt13 (~3.6056)");
     System.out.println( distance(p1,p3));
     System.out.println( Point.distance(p3,p1));
-    System.out.println( p1.distanceTo(p3));
+    System.out.println( p1.distanceTo(p3) + "\n");
 
+    System.out.println("distance methods test3: expected sqrt41 (~6.4031)");
     System.out.println( distance(p3,p2));
     System.out.println( Point.distance(p2,p3));
-    System.out.println( p3.distanceTo(p2));
+    System.out.println( p3.distanceTo(p2) + "\n");
 
   }
 }
