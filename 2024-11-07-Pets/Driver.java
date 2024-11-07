a6cdfda0bf53723e94c2f5ae6502bc3799f5155d
Nicolas Marchese <nicktmarchese@gmail.com>
2024-11-07 10:41:29 -0500

    up to step 8

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,8 @@ public class Driver{
   public static void main(String[] args){
     Animal dog = new Animal("woof", 1, "bob");
     dog.speak();
+    
+    Bird birb = new Bird("caw caw", 1, "birby", 5.5, "red");
+    birb.speak();
   }
 }
