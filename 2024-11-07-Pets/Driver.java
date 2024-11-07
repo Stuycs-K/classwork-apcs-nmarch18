b61cde719a948c39b3cbf94bf7275fda8aae5a86
Nicolas Marchese <nicktmarchese@gmail.com>
2024-11-07 10:50:01 -0500

    up to step 11

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,9 +1,22 @@
+/* STEP 8:
+super.getName() returns a string which is then added to the Bird getName().
+*/
+/* STEP 10:
+The only one that didn't work was declaring a Bird as a new Animal; this makes sense
+since the Animal class has a has-a relationship with the Bird class, not a is-a one. */
+
 public class Driver{
   public static void main(String[] args){
     Animal dog = new Animal("woof", 1, "bob");
     dog.speak();
-    
+
     Bird birb = new Bird("caw caw", 1, "birby", 5.5, "red");
     birb.speak();
+
+    Animal a1 = new Animal("meow", 12, "mittens");
+    Bird b1 = new Bird("ahhahahah", 50, "pigeon", 8.1, "white");
+    // Bird b2 = new Animal("gimme a cracker", 9, "polly", 0.1, "green"); ERROR
+    Animal a2 = new Bird("im not a bird", 16, "nick", 71, "white");
+
   }
 }
