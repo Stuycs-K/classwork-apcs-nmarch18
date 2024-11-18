5de90d3dddfaec584f74a1d7ef268cb33b026c33
Nicolas Marchese <nicktmarchese@gmail.com>
2024-11-18 10:56:46 -0500

    end of class 11/18

--- 2024-11-18-advent2016/day1tester.java
+++ 2024-11-18-advent2016/day1tester.java
@@ -2,8 +2,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TriangleTester{
+public class day1tester{
   public static int distanceA(String filename){
+    try{
+      int[] currentLine = new int[3];
+      int xPos = 0;
+      int yPos = 0;
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
 
+      input.split(", ");
+
+      
+
+
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+      return -1;
+
+    }
   }
-}
