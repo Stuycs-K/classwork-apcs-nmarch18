07f6ae0ae9b3900c13fa472c335e93cc5b5794dc
Nicolas Marchese <nicktmarchese@gmail.com>
2024-11-18 18:12:03 -0500

    finished day 2

--- 2024-11-18-advent2016/Day2/day2driver.java
+++ 2024-11-18-advent2016/Day2/day2driver.java
@@ -1,5 +1,6 @@
-public class day1driver{
+public class day2driver{
   public static void main (String[] args){
-    System.out.println(day2tester.getCode("day2input.txt"));
+    System.out.println(day2tester.getCodeA("day2input.txt"));
+    System.out.println(day2tester.getCodeB("day2input.txt"));
   }
 }
