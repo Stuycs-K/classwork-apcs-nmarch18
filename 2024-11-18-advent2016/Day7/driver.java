96c2c1a1462828ebd1d25bf28ad952639a556682
Nicolas Marchese <nicktmarchese@gmail.com>
2024-11-19 21:34:49 -0500

    finished day 7

--- 2024-11-18-advent2016/Day7/driver.java
+++ 2024-11-18-advent2016/Day7/driver.java
@@ -1,5 +1,6 @@
 public class driver{
   public static void main(String[] args){
-    System.out.println(tester.countIPs("input.txt"));
+    System.out.println(tester.countABBA("input.txt"));
+    System.out.println(tester.countSSL("input.txt"));
   }
 }
--- 2024-11-18-advent2016/Day7/tester.java
+++ 2024-11-18-advent2016/Day7/tester.java
@@ -4,11 +4,33 @@ import java.util.*;
 
 public class tester{
 
-  public static boolean validate(String ip){
+  public static boolean validateABBA(String ip){
+
+    boolean inHypernet = false;
+    boolean hasABBA = false;
+
+    for (int i = 0; i < ip.length() - 3; i++){
+      if (ip.charAt(i+3) == '['){
+        inHypernet = true;
+      }
+      if (ip.charAt(i+3) == ']'){
+        inHypernet = false;
+      }
+      if (ip.charAt(i) == ip.charAt(i+3) && ip.charAt(i+1) == ip.charAt(i+2) && ip.charAt(i) != ip.charAt(i+1)){
+        if (inHypernet){
+          return false;
+        }
+        else{
+          hasABBA = true;
+        }
+      }
+    }
+
+    return hasABBA;
 
   }
 
-  public static int countIPs(String filename){
+  public static int countABBA(String filename){
 
     try{
 
@@ -19,7 +41,7 @@ public class tester{
 
       while (input.hasNextLine()){
         ip = input.nextLine();
-        if (validate(ip)){
+        if (validateABBA(ip)){
           count += 1;
         }
       }
@@ -34,4 +56,66 @@ public class tester{
 
   }
 
+  public static boolean validateSSL(String ip){
+
+    boolean inHypernet = false;
+    boolean hasSSL = false;
+    char a = ' ';
+    char b = ' ';
+
+    for (int i = 0; i < ip.length() - 2; i++){
+      if (ip.charAt(i+2) == '['){
+        inHypernet = true;
+      }
+      if (ip.charAt(i+2) == ']'){
+        inHypernet = false;
+      }
+      if (!inHypernet && ip.charAt(i) == ip.charAt(i+2) && Character.isAlphabetic(ip.charAt(i+1))){
+        a = ip.charAt(i);
+        b = ip.charAt(i+1);
+
+        for (int j = 0; j < ip.length() - 2; j++){
+
+          if (ip.charAt(j+2) == '['){
+            inHypernet = true;
+          }
+
+          if (ip.charAt(j+2) == ']'){
+            inHypernet = false;
+          }
+
+          if (inHypernet && ip.charAt(j) == b && ip.charAt(j+1) == a && ip.charAt(j+2) == b){
+            hasSSL = true;
+          }
+        }
+      }
+    }
+    return hasSSL;
+  }
+
+  public static int countSSL(String filename){
+
+    try{
+
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int count = 0;
+      String ip;
+
+      while (input.hasNextLine()){
+        ip = input.nextLine();
+        if (validateSSL(ip)){
+          count += 1;
+        }
+      }
+
+      return count;
+
+    }
+
+    catch(FileNotFoundException e){
+      return 0;
+    }
+
+  }
 }
