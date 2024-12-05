c91162a8106f33c6a6f6c7f14b473018f50c73a9
Nicolas Marchese <nicktmarchese@gmail.com>
2024-12-05 01:12:06 -0500

    added bright method

--- 2024-12-04-ColorScreen/Text.java
+++ 2024-12-04-ColorScreen/Text.java
@@ -1,28 +1,11 @@
 public class Text{
-//MODIFIERS
-public static final int BRIGHT = 1;
-public static final int DARK = 2;
-public static final int ITALICS = 3;
-//COLORS
-public static final int BLACK = 30;
-public static final int RED = 31;
-public static final int GREEN = 32;
-public static final int YELLOW = 33;
-public static final int BLUE = 34;
-public static final int MAGENTA = 35;
-public static final int CYAN = 36;
-public static final int WHITE = 37;
-//CONTROLS
-public static final String CLEAR_SCREEN =  "\033[2J";
-public static final String HIDE_CURSOR =  "\033[?25l";
-public static final String SHOW_CURSOR =  "\033[?25h";
-
-//use this to go back to normal terminal before exiting.
-public static final String RESET = "\033[0m"+SHOW_CURSOR;
 
 //use this to convert from color to background (30 to 37 becomes 40 to 47)
-public static int  background(int color){
+public static int background(int color){
   return color + 10;
+}
+public static int bright(int color){
+  return color + 60;
 }
 
 //terminal specific character to move the cursor to a location
