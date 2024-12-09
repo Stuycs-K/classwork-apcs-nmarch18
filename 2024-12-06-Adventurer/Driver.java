6f6ef26e1dca19ae6a285cf631cb16d5a6be056b
Nicolas Marchese <nicktmarchese@gmail.com>
2024-12-09 00:48:37 -0500

    finalized testing (DONE)

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Driver{
   public static void main(String[] args){
-    Adventurer p1 = new Wizard("Wizard 1", 25, 0);
-    Adventurer p2 = new Wizard("Wizard 2", 25, 0);
+
+    // To test our subclass, we'll make a duel between two Wizards.
+
+    Adventurer p1 = new Wizard("Al", 23, 3);
+    Adventurer p2 = new Wizard("Bob", 25, 5);
     int turn = 0;
     System.out.println();
 
@@ -10,22 +15,40 @@ public class Driver{
         int t = (int)(turn/2 + 1);
         System.out.println("* TURN " + t + " *");
       }
-      if (turn % 6 == 0){
+
+      // attack: randomly deals 1-5 damage.
+      // support: randomly gains 1-3 health and 1-3 mana.
+      // specialAttack: 5 mana (max) required. randomly deals 6-10 damage to other wizard but 1-5 damage to yourself.
+
+      // Al's cycle: attack -> support -> attack -> specialAttack
+      // Bob's cycle: specialAttack -> attack -> attack -> support
+
+      // Who would win this duel?
+      // Well, it turns out, with 10000 simulations, that:
+      // Al won 45.4%, Bob won 42.9%, and 11.7% ended in a draw (with both dying to the same fireball).
+
+      if (turn % 8 == 0){
         System.out.println(p1.attack(p2));
       }
-      if (turn % 6 == 1){
+      if (turn % 8 == 1){
         System.out.println(p2.specialAttack(p1));
       }
-      if (turn % 6 == 2){
+      if (turn % 8 == 2){
         System.out.println(p1.support());
       }
-      if (turn % 6 == 3){
+      if (turn % 8 == 3){
         System.out.println(p2.attack(p1));
       }
-      if (turn % 6 == 4){
+      if (turn % 8 == 4){
+        System.out.println(p1.attack(p2));
+      }
+      if (turn % 8 == 5){
+        System.out.println(p2.attack(p1));
+      }
+      if (turn % 8 == 6){
         System.out.println(p1.specialAttack(p2));
       }
-      if (turn % 6 == 5){
+      if (turn % 8 == 7){
         System.out.println(p2.support());
       }
 
@@ -54,6 +77,6 @@ public class Driver{
       }
       turn++;
     }
-
   }
+
 }
